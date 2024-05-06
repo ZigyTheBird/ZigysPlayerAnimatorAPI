f90ac7a8e99e827a8b70390681ac35611f0bade7
ZigyWasTaken <imanyarandi222@gmail.com>
2024-05-06 18:05:23 +0330

    Release 1.3.4
    Fixed issue #4

--- common/src/main/java/zigy/playeranimatorapi/mixin/PlayerMixin_azureOnly.java
+++ common/src/main/java/zigy/playeranimatorapi/mixin/PlayerMixin_azureOnly.java
@@ -1,6 +1,5 @@
 package zigy.playeranimatorapi.mixin;
 
-import mod.azure.azurelib.animatable.GeoEntity;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animation.AnimatableManager;
 import mod.azure.azurelib.core.animation.AnimationController;
@@ -9,22 +8,23 @@ import mod.azure.azurelib.util.AzureLibUtil;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import zigy.playeranimatorapi.GeoPlayer;
 import zigy.playeranimatorapi.ModInit;
 import zigy.playeranimatorapi.azure.ModAzureUtilsClient;
 
-@Mixin(Player.class)
-public abstract class PlayerMixin_azureOnly implements GeoEntity {
+@Mixin(value = Player.class, remap = false)
+public abstract class PlayerMixin_azureOnly implements GeoPlayer {
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, ModInit.MOD_ID, state -> PlayState.STOP).setOverrideEasingTypeFunction((player) -> ModAzureUtilsClient.getEasingTypeForID((Player)(Object)this)));
+        controllers.add(new AnimationController<>((Player)(Object)this, ModInit.MOD_ID, state -> PlayState.STOP).setOverrideEasingTypeFunction((player) -> ModAzureUtilsClient.getEasingTypeForID((Player)(Object)this)));
     }
 
     @Unique
-    private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
+    private final AnimatableInstanceCache zigysPlayerAnimatorAPI$cache = AzureLibUtil.createInstanceCache(this);
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
+        return zigysPlayerAnimatorAPI$cache;
     }
 }
