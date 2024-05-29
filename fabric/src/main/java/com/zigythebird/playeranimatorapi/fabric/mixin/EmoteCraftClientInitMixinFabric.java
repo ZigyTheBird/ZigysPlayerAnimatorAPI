f71597054b27472c58b092e053883f02244215af
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2024-05-29 04:01:31 +0330

    A very small amount of code cleanup

--- fabric/src/main/java/com/zigythebird/playeranimatorapi/fabric/mixin/EmoteCraftClientInitMixinFabric.java
+++ fabric/src/main/java/com/zigythebird/playeranimatorapi/fabric/mixin/EmoteCraftClientInitMixinFabric.java
@@ -18,13 +18,6 @@ import com.zigythebird.playeranimatorapi.playeranims.CustomModifierLayer;
 
 @Mixin(ClientInit.class)
 public abstract class EmoteCraftClientInitMixinFabric {
-    @Unique
-    private static final ResourceLocation animationLayerId = new ResourceLocation(ModInit.MOD_ID, "factory");
-
-    @Unique
-    private static CustomModifierLayer animationContainer(AbstractClientPlayer player) {
-        return (CustomModifierLayer) PlayerAnimationAccess.getPlayerAssociatedData(player).get(animationLayerId);
-    }
 
     @Redirect(method = "lambda$initKeyBinding$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"))
     private static void initKeybinding(Minecraft instance, @Nullable Screen guiScreen) {
