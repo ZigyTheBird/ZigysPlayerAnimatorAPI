b76be83d645580e164f8c27c648a7f7cd455b8f5
ZigyWasTaken <imanyarandi222@gmail.com>
2024-06-12 00:39:10 +0330

    Port release 2.0.1 to 1.20.4
    Fixed a bug where the Player Model Overlay would break in the inventory GUI.

--- common/src/main/java/com/zigythebird/playeranimatorapi/azure/PlayerAnimationModel.java
+++ common/src/main/java/com/zigythebird/playeranimatorapi/azure/PlayerAnimationModel.java
@@ -1,8 +1,7 @@
 package com.zigythebird.playeranimatorapi.azure;
 
 import com.zigythebird.playeranimatorapi.playeranims.PlayerAnimations;
-import mod.azure.azurelib.core.animatable.GeoAnimatable;
-import mod.azure.azurelib.model.GeoModel;
+import mod.azure.azurelib.common.api.client.model.GeoModel;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
