9fae0094d82e32d55ba23c21c06f89ddbfee2fe4
ZigyWasTaken <imanyarandi222@gmail.com>
2024-06-29 23:48:50 +0330

    Release 2.0.5
    Fixed an issue when playing a replay using the ReplayMod.
    Fixed an dev environment issue.

--- fabric/src/main/java/com/zigythebird/playeranimatorapi/compatibility/fabric/ReplayModCompatImpl.java
+++ fabric/src/main/java/com/zigythebird/playeranimatorapi/compatibility/fabric/ReplayModCompatImpl.java
@@ -18,18 +18,22 @@ import java.util.List;
 
 public class ReplayModCompatImpl {
     public static void playPlayerAnim(AbstractClientPlayer player, ResourceLocation animationID, PlayerParts parts, List<CommonModifier> modifiers, int fadeLength, int easeID, int priority, boolean firstPersonEnabled, boolean replaceTick) {
-        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
-        PlayerAnimationData data = new PlayerAnimationData(player.getUUID(), animationID,
-                parts, modifiers, fadeLength, easeID, priority, firstPersonEnabled);
-        buf.writeUtf(PlayerAnimAPI.gson.toJson(PlayerAnimationData.CODEC.encodeStart(JsonOps.INSTANCE, data).getOrThrow(true, ModInit.LOGGER::warn)));
-        buf.writeBoolean(replaceTick);
-        ReplayModRecording.instance.getConnectionEventHandler().getPacketListener().save(ServerPlayNetworking.createS2CPacket(PlayerAnimatorAPIClientFabric.altPlayPlayerAnimationPacket, buf));
+        if (ReplayModRecording.instance.getConnectionEventHandler().getPacketListener() != null) {
+            FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+            PlayerAnimationData data = new PlayerAnimationData(player.getUUID(), animationID,
+                    parts, modifiers, fadeLength, easeID, priority, firstPersonEnabled);
+            buf.writeUtf(PlayerAnimAPI.gson.toJson(PlayerAnimationData.CODEC.encodeStart(JsonOps.INSTANCE, data).getOrThrow(true, ModInit.LOGGER::warn)));
+            buf.writeBoolean(replaceTick);
+            ReplayModRecording.instance.getConnectionEventHandler().getPacketListener().save(ServerPlayNetworking.createS2CPacket(PlayerAnimatorAPIClientFabric.altPlayPlayerAnimationPacket, buf));
+        }
     }
 
     public static void stopPlayerAnim(AbstractClientPlayer player, ResourceLocation animationID) {
-        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
-        buf.writeUUID(player.getUUID());
-        buf.writeResourceLocation(animationID);
-        ReplayModRecording.instance.getConnectionEventHandler().getPacketListener().save(ServerPlayNetworking.createS2CPacket(PlayerAnimatorAPIClientFabric.altStopPlayerAnimationPacket, buf));
+        if (ReplayModRecording.instance.getConnectionEventHandler().getPacketListener() != null) {
+            FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+            buf.writeUUID(player.getUUID());
+            buf.writeResourceLocation(animationID);
+            ReplayModRecording.instance.getConnectionEventHandler().getPacketListener().save(ServerPlayNetworking.createS2CPacket(PlayerAnimatorAPIClientFabric.altStopPlayerAnimationPacket, buf));
+        }
     }
 }
