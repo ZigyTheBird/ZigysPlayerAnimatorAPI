4d21d6a78953ea138dd3a9f8e88d84016c42c66b
ZigyWasTaken <imanyarandi222@gmail.com>
2024-08-10 22:36:33 +0330

    Release 2.1.0

---
+++ common/src/main/java/com/zigythebird/playeranimatorapi/registry/GeckoControllerRegistry.java
@@ -0,0 +1,43 @@
+package com.zigythebird.playeranimatorapi.registry;
+
+import com.zigythebird.playeranimatorapi.ModInit;
+import com.zigythebird.playeranimatorapi.gecko.AnimatablePlayerLayer;
+import com.zigythebird.playeranimatorapi.playeranims.CustomModifierLayer;
+import net.minecraft.core.NonNullList;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.animation.PlayState;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class GeckoControllerRegistry {
+    private static final Map<String, NonNullList<BiFunction<AnimationState<AnimatablePlayerLayer>, CustomModifierLayer<?>, PlayState>>>  CONTROLLERS = new HashMap<>();
+
+    public static AnimationController.AnimationStateHandler<AnimatablePlayerLayer> getControllerForMod(int i, CustomModifierLayer<?> layer) {
+        try {
+            if (layer.isActive()) {
+                String modID = layer.data.animationID().getNamespace();
+                return (state) -> CONTROLLERS.get(modID).get(i).apply(state, layer);
+            }
+            else {
+                return (state) -> PlayState.CONTINUE;
+            }
+        }
+        catch (Exception e) {
+            return (state) -> PlayState.CONTINUE;
+        }
+    }
+
+    public static void addController(String modID, BiFunction<AnimationState<AnimatablePlayerLayer>, CustomModifierLayer<?>, PlayState> controller) {
+        if (!CONTROLLERS.containsKey(modID)) {
+            CONTROLLERS.put(modID, NonNullList.createWithCapacity(5));
+        }
+        if (CONTROLLERS.get(modID).size() >= 5) {
+            ModInit.LOGGER.error("Cannot register new gecko controller for mod " + modID + " due to a controller count limit of 5.");
+            return;
+        }
+        CONTROLLERS.get(modID).add(controller);
+    }
+}
